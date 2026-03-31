64f50b65ab536b7310b87b9c38e460a280a54b59
ziaFBacht <myfaizbachtiar10@gmail.com>
2026-03-31 12:52:41 +0700

    Changed a line

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="bg-blue-200">Vite + React</h1>
+      <h1>Vite + React</h1>
       <h1>Faiz Bachtiar Rayhananto</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
