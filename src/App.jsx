6a15cd1389c0b4134184a977e41f2d1d153cb54c
ziaFBacht <myfaizbachtiar10@gmail.com>
2026-03-31 13:53:44 +0700

    menggunakan variant class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const courses = [
+    {
+      title: "System Administration and IT Infrastructure Services",
+    },
+    {
+      title: "Operating Systems Becoming a Power User",
+    },
+    {
+      title: "The Bits and Bytes of Computer Networking",
+    },
+    {
+      title: "Technical Support Fundamentals",
+    },
+    {
+      title: "How to Succeed at: Writing Applications",
+    },
+    {
+      title: "Medicine Administration for Carers",
+    },
+  ];
 
   return (
     <>
-    <div className="min-h-screen bg-neutral-800
-      flex flex-col items-center justify-center text-center">
-      <div className="flex items-center justify-center space-x-8 mb-12">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-24 h-24 animate-spin" alt="React logo" />
-        </a>
+      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+        {courses.map((course, index) => (
+          <div key={index} className="shadow rounded-lg overflow-hidden flex flex-col
+          hover:border hover:border-gray-500
+          hover:shadow-lg hover:scale-105 transition-transform duration-300">
+            {/* Image */}
+            <img
+              src="https://www.placehold.co/300x200"
+              alt={course.title}
+              className="w-full h-80 object-cover"
+            />
+
+            {/* Content */}
+            <div className="p-4 bg-red-100 flex flex-col justify-between h-full">
+              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
+
+              <div className="bg-red-50 p-2 rounded-lg">
+                <div className="text-sm text-gray-500 mt-2 flex justify-between">
+                  <span>👥 123 users</span>
+                  <span>⏱ 60</span>
+                </div>
+
+                {/* Author */}
+                <div className="mt-3 flex items-center gap-2">
+                  <img
+                    src="https://www.placehold.co/50x50"
+                    alt="Author's Avatar"
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                  <div>
+                    <p className="text-sm font-medium">Author's Name</p>
+                    <p className="text-xs text-gray-500">Designer</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Footer */}
+              <div className="mt-4 flex items-center justify-between">
+                <div className="bg-white py-2 px-4 rounded">$123</div>
+                <button className="bg-red-700 hover:bg-red-900 active:bg-red-500
+                 text-white px-4 py-2 rounded text-sm">
+                  Get Started
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1 className="text-white text-6xl font-bold mb-16">Vite + React</h1>
-      <h1 className='text-neutral-600'>Faiz Bachtiar Rayhananto</h1>
-      <div className="text-sm space-y-6 mb-8">
-        <button onClick={() => setCount((count) => count + 1)} className=' bg-neutral-900 text-white py-2 px-6 rounded-lg text-lg'>
-          count is {count}
-        </button>
-        <p className="text-neutral-400">  
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-neutral-500">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
