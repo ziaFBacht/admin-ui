83b57d7b0a4bd4a92b6e9e9a1e1079b90c350ee5
ziaFBacht <myfaizbachtiar10@gmail.com>
2026-03-31 13:12:27 +0700

    menggunakan utility class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,30 @@ function App() {
 
   return (
     <>
-      <div>
+    <div className="min-h-screen bg-neutral-800
+      flex flex-col items-center justify-center text-center">
+      <div className="flex items-center justify-center space-x-8 mb-12">
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-24 h-24 animate-spin" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <h1>Faiz Bachtiar Rayhananto</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <h1 className="text-white text-6xl font-bold mb-16">Vite + React</h1>
+      <h1 className='text-neutral-600'>Faiz Bachtiar Rayhananto</h1>
+      <div className="text-sm space-y-6 mb-8">
+        <button onClick={() => setCount((count) => count + 1)} className=' bg-neutral-900 text-white py-2 px-6 rounded-lg text-lg'>
           count is {count}
         </button>
-        <p>
+        <p className="text-neutral-400">  
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-neutral-500">
         Click on the Vite and React logos to learn more
       </p>
+    </div>
     </>
   )
 }
